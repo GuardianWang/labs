1b327ee3deb4a91ea4922e5c5e03de06cb8dfac2
guardianwang <962247283@qq.com>
2021-11-11 18:44:58 -0500

    shift

--- lab09/filter/FilterShift.cpp
+++ lab09/filter/FilterShift.cpp
@@ -2,10 +2,18 @@
 #include "FilterUtils.h"
 
 FilterShift::FilterShift(ShiftDirection direction):
-    m_shiftDir(direction)
+    m_shiftDir(direction),
+    m_kernel(std::vector<float>(9, 0.f))
 {
     // TODO: Task 19 Initialize kernel for shift
-
+    switch (m_shiftDir) {
+    case ShiftDirection::SHIFT_LEFT:
+        *(m_kernel.begin() + m_kernel.size() / 2 + 1) = 1.f;
+        break;
+    case ShiftDirection::SHIFT_RIGHT:
+        *(m_kernel.begin() + m_kernel.size() / 2 - 1) = 1.f;
+        break;
+    }
 }
 
 FilterShift::~FilterShift()
@@ -14,5 +22,6 @@ FilterShift::~FilterShift()
 
 void FilterShift::apply(Canvas2D *canvas) {
     // TODO: Task 18 Call convolve function from superclass
+    FilterUtils::Convolve2D(canvas->data(), canvas->width(), canvas->height(), m_kernel);
 
 }
