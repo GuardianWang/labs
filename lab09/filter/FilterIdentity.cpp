247640127eff79e0bc07c1234dfbd26d60c8035a
guardianwang <962247283@qq.com>
2021-11-11 18:29:37 -0500

    convolution

--- lab09/filter/FilterIdentity.cpp
+++ lab09/filter/FilterIdentity.cpp
@@ -1,10 +1,12 @@
 #include "FilterIdentity.h"
 #include "FilterUtils.h"
 
-FilterIdentity::FilterIdentity()
+FilterIdentity::FilterIdentity() :
+    m_kernel(std::vector<float>(9, 0.f))
 {
     // TODO: Task 17 Initialize kernel for identity
-
+    int kerSize = m_kernel.size();
+    m_kernel[kerSize / 2] = 1.f;
 }
 
 FilterIdentity::~FilterIdentity()
@@ -13,5 +15,6 @@ FilterIdentity::~FilterIdentity()
 
 void FilterIdentity::apply(Canvas2D *canvas) {
     // TODO: Task 16 Call convolve function from superclass
+    FilterUtils::Convolve2D(canvas->data(), canvas->width(), canvas->height(), m_kernel);
 
 }
--- lab09/filter/FilterUtils.cpp
+++ lab09/filter/FilterUtils.cpp
@@ -1,5 +1,6 @@
 #include "FilterUtils.h"
 #include <algorithm>
+#include <numeric>
 #include "math.h"
 #include <cstring>
 
@@ -12,33 +13,58 @@ inline unsigned char REAL2byte(float f) {
 
 void Convolve2D(RGBA* data, int width, int height, const std::vector<float> &kernel) {
     // TODO: Task 9 Create buffer to store new image data
+    std::vector<RGBA> result;
+    int imgSize = width * height;
+    result.reserve(imgSize);
 
 
     // TODO: Task 10 Obtain kernel dimension
-
+    int kerLen = std::sqrt(kernel.size());
+    int kerLenHalf = kerLen / 2;
+    size_t centerIndex = 0;
+    int kerSum = std::accumulate(kernel.cbegin(), kernel.cend(), 0);
+    float kerSumInv = 1.f / kerSum;
 
     for (int r = 0; r < height; r++) {
-        for (int c = 0; c < width; c++) {
-            size_t centerIndex = r * width + c;
+        for (int c = 0; c < width; c++, centerIndex++) {
 
             // TODO: Task 11 Initialize color to accumulate convolution data
-
+            float red, green, blue;
+            red = green = blue = 0.f;
 
             // TODO: Task 12
             // Iterate over the kernel using the value from task 10
             // Obtain the value at current index of kernel
             // Find correct index in original image data
             // Accumulate the kernel applied to pixel value in color_acc
-
-            // TODO: Task 13 Ignore outside bounds
-
-
+            size_t kerId = 0;
+            for (int kr = -kerLenHalf; kr <= kerLenHalf; kr++) {
+                for (int kc = -kerLenHalf; kc <= kerLenHalf; kc++, kerId++) {
+                    int currR = r + kr;
+                    int currC = c + kc;
+                    if ((currR < 0) || (currR >= height) ||
+                            (currC < 0 || (currC) >= width)) {
+                        // TODO: Task 13 Ignore outside bounds
+                        continue;
+                    }
+                    int currId = width * currR + currC;
+                    red += data[currId].r * kernel[kerId];
+                    green += data[currId].g * kernel[kerId];
+                    blue += data[currId].b * kernel[kerId];
+                }
+            }
+            red *= kerSumInv;
+            green *= kerSumInv;
+            blue *= kerSumInv;
             // TODO: Task 14 Update buffer with accumulated color
-
+            result.emplace_back(static_cast<unsigned char>(red + 0.5f),
+                                static_cast<unsigned char>(green + 0.5f),
+                                static_cast<unsigned char>(blue + 0.5f));
         }
     }
 
     // TODO: Task 15 Copy over buffer to canvas data
+    std::memcpy(data, result.data(), sizeof (RGBA) * imgSize);
 
 }
 
